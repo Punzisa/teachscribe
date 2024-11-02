88af2e8e62c68a011e6d2df1b9362ed71964e9f2
unclebinary1001 <nzundamahlangu@gmail.com>
2024-11-01 22:58:36 -0500

    fix: only allow images for image picker

--- components/ProfilePage/Profile.tsx
+++ components/ProfilePage/Profile.tsx
@@ -77,7 +77,7 @@ const Profile = () => {
 
   const pickImage = async () => {
     const result = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
       allowsEditing: true,
       aspect: [4, 4],
       quality: 1,
