db710a3b36282e63361196c3cd094d0e22f2969a
unclebinary1001 <nzundamahlangu@gmail.com>
2025-01-21 23:10:14 -0600

    refactor: update lesson plan data

--- components/LessonsPage/ExportLessonPdf.tsx
+++ components/LessonsPage/ExportLessonPdf.tsx
@@ -2,7 +2,7 @@ import CourtOfArms from '@/assets/images/court_of_arms.png'
 import * as FileSystem from 'expo-file-system'
 import { shareAsync } from 'expo-sharing'
 import { Asset } from 'expo-asset'
-import { LessonData } from '../forms/LessonPlan/LessonPlan'
+import { LearningDevelopment, LessonData } from '../forms/LessonPlan/LessonPlan'
 import { ProfileData } from '../ProfilePage/Profile'
 import * as Print from 'expo-print'
 
@@ -45,13 +45,14 @@ export const generateLessonHTML = async (lessonData: LessonData, teacherProfile:
               .content-wrapper {
               flex: 1 0 auto;
               margin: 5px;
+              padding-left: 20px;
             }
               .heading {
               text-align: center;
             }
             h1 { color: #333; }
-            .lesson-info { margin-bottom: 20px; padding: 20px 0; }
-            .lesson-content { line-height: 1.4; }
+            .lesson-info { margin-bottom: 5px; padding: 10px 0; }
+            .lesson-content { line-height: 1.2; }
             .footer {
               flex-shrink: 0;
               padding: 10px 20px;
@@ -59,6 +60,19 @@ export const generateLessonHTML = async (lessonData: LessonData, teacherProfile:
               font-size: 12px;
               color: #666;
               border-top: 1px solid #eee;
+            }
+              table, td, th {  
+              border: 1px solid #ddd;
+              text-align: left;
+            }
+            table {
+              border-collapse: collapse;
+              width: 95%;
+               margin: 0 auto;
+
+            }
+            th, td {
+              padding: 5px;
             }
           </style>
         </head>
@@ -71,34 +85,58 @@ export const generateLessonHTML = async (lessonData: LessonData, teacherProfile:
           <h3>Lesson Plan</h3>
           </div>
           <div class="lesson-info">
-            <p><strong>Teacher Name:</strong> ${teacherProfile.salutation} ${teacherProfile.firstName} ${teacherProfile.lastName}</p>
             <p><strong>Date:</strong> ${lessonData.date.toDateString()}</p>
-            <p><strong>Time:</strong> ${lessonData.date.toLocaleTimeString()}</p>
+            <p><strong>Teacher Name:</strong> ${teacherProfile.salutation} ${teacherProfile.firstName} ${teacherProfile.lastName}</p>
             <p><strong>Class:</strong> ${lessonData.class}</p>
+            <p><strong>Subject:</strong> ${lessonData.subject}</p>
+            <p><strong>Title:</strong> ${lessonData.title}</p>
+            <p><strong>Time:</strong> ${lessonData.date.toLocaleTimeString()}</p>
             <p><strong>Duration:</strong> ${lessonData.duration}</p>
+            <p><strong>Resources:</strong> ${lessonData.resources}</p>
+            <p><strong>References:</strong> ${lessonData.resources}</p>
           </div>
 
           <div class="lesson-content">
-          <h4>Description:</h4>
-            ${lessonData.description}
-            <h4>Objectives:</h4>
-            <ul>
-              ${lessonData.objectives.map((objective) => `<li>${objective}</li>`).join('')}
-            </ul>
-            <h4>Activities:</h4>
-            <h5>Teacher activities:</h5>
-            ${lessonData.activities.teachingActivities}
-            <h5>Pupil activities:</h5>
-            ${lessonData.activities.pupilActivities}
-            <h5>Teaching aids:</h5>
-            ${lessonData.activities.teachingAids}
+            <h4>Description:</h4>
+              ${lessonData.description}
+            <h4>Major Learning Outcome:</h4>
+              ${lessonData.majorLearningOutcome}
+            <h4>Evidence of Attainment:</h4>
+              ${lessonData.evidenceOfAttainment}
+            <h4>Rationale:</h4>
+              ${lessonData.rationale}
             
+            <h4>Lesson Development</h4>
+            <table>
+              <tr>
+                <th>Time</th>
+                <th>Teacher Activities</th>
+                <th>Learner Activities</th>
+              </tr>
+              ${lessonData.learningDevelopmentEntries
+                .map(
+                  (entry: LearningDevelopment) => `
+                <tr>
+                  <td>${entry.time}</td>
+                  <td>${entry.teacherActivites}</td>
+                  <td>${entry.learnerActivities}</td>
+                </tr>
+              `
+                )
+                .join('')}
+                </tr>
+            </table>
+        
+            <h4>Teacher Evaluation</h4>
+            ${lessonData.teacherEvaluation}
+            <h4>Pupil Evaluation</h4>
+            ${lessonData.pupilEvaluation}
             
           </div>
-          </div>
-          <footer class="footer">
+        </div>
+        <footer class="footer">
           Powered by TeachScribe
-          </footer>
+        </footer>
         </body>
       </html>
     `
