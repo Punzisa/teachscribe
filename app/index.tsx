7c9966a27ace4409e81a346e90f533c3027fc3dd
unclebinary1001 <nzundamahlangu@gmail.com>
2025-02-20 03:53:50 -0600

    fix: never go to (auth) route when user is signedIn

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,13 @@
+import { useAuth } from '@clerk/clerk-expo'
 import { Redirect } from 'expo-router'
 
 const StartPage = () => {
+  const { isLoaded, isSignedIn } = useAuth()
+
+  if (isSignedIn && isLoaded) {
+    return <Redirect href="/(tabs)" />
+  }
+
   return <Redirect href="/(auth)" />
 }
 
